d69a5afda0fd0ef7238ee0c0dc19bbc1e0bb5204
queenbellabots-cloud <queenbellabots@gmail.com>
2026-07-29 12:08:42 +0300

    Create settings.js

---
+++ settings.js
@@ -0,0 +1,20 @@
+const settings = {
+  prefix: ".", // 👈 Command prefix
+  packname: '👑 QUEEN BELLA MD 👑',
+  author: '𝐑𝐎𝐃𝐆𝐄𝐑𝐒 𝐎𝐍𝐘𝐀𝐍𝐆𝐎',
+  botName: "👑 QUEEN BELLA MD 👑",
+  botOwner: '𝐑𝐎𝐃𝐆𝐄𝐑𝐒 𝐎𝐍𝐘𝐀𝐍𝐆𝐎',
+  ownerNumber: '254755660053',
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  maxStoreMessages: 20, 
+  storeWriteInterval: 10000,
+  description: "QUEEN BELLA MD - A Multi-Device WhatsApp Bot By Dev RODGERS",
+  version: "1.0.0",
+  updateZipUrl: "https://github.com/YOUR_USERNAME/QUEEN-BELLA-MD/archive/refs/heads/main.zip",
+};
+
+// 👇 Set global prefix
+global.prefix = settings.prefix; 
+
+module.exports = settings;

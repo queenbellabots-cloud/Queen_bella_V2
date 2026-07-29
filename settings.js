b3299f859e03f73ac9167baf1c2ac9eee6df06fc
queenbellabots-cloud <queenbellabots@gmail.com>
2026-07-29 11:12:56 +0300

    Create settings.js

---
+++ settings.js
@@ -0,0 +1,26 @@
+const settings = {
+  prefix: ".", // 👈 Command prefix
+  BOT_NAME: "𝐐𝐔𝐄𝐄𝐍 𝐁𝐄𝐋𝐋𝐀 𝐕𝟏",
+  BOT_VERSION: "1.0",
+  OWNER_NAME: "𝐑𝐎𝐃𝐆𝐄𝐑𝐒 𝐎𝐍𝐘𝐀𝐍𝐆𝐎",
+  OWNER_NUMBER: "254755660053", // Your WhatsApp number (no + or spaces)
+  DEVELOPER: "𝐑𝐎𝐃𝐆𝐄𝐑𝐒 𝐎𝐍𝐘𝐀𝐍𝐆𝐎",
+  MODE: "PUBLIC",
+  autoViewStatus: true,
+  autoLikeStatus: true,
+  autoStatusSeen: true,
+  autoStatusReact: true,
+  autoStatusReply: false,
+  autoRecording: false,
+  antiCall: false,
+  readMessages: false,
+  autoReact: false,
+  workType: "public",
+  botImage: "https://i.imgur.com/687ZxLW.jpeg",
+  description: "𝐐𝐔𝐄𝐄𝐍 𝐁𝐄𝐋𝐋𝐀 𝐌𝐃 𝐕𝟏 - Multi-Device WhatsApp Bot By 𝐑𝐎𝐃𝐆𝐄𝐑𝐒 𝐎𝐍𝐘𝐀𝐍𝐆𝐎",
+};
+
+// 👇 Set global prefix
+global.prefix = settings.prefix; 
+
+module.exports = settings;

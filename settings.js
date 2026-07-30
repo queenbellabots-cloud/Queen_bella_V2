e968e9da7ac517a4721155e0f0f9cf90f9ab62b5
queenbellabots-cloud <queenbellabots@gmail.com>
2026-07-30 19:23:44 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,19 +5,18 @@ const settings = {
   ownerNumber: "254755660053",
   commandMode: "public",
   usePairingCode: true,
+  autoRead: true,
+  
+  // Channel Info
   channelId: "120363423209691396@newsletter",
   channelName: "👑 QUEEN BELLA MD 👑",
   channelLink: "https://whatsapp.com/channel/0029VbCwZHACXC3PNHgtMT31",
-  // Menu images (will rotate)
-  menuImages: [
-    "https://imagetourl.cloud/9eumy3kr.jpg",
-    "https://imagetourl.cloud/jey865he.jpg",
-    "https://imagetourl.cloud/8uafyai1.jpg"
-  ],
-  welcomeImages: [
-    "https://imagetourl.cloud/jey865he.jpg",
-    "https://imagetourl.cloud/8uafyai1.jpg"
-  ],
+  
+  // Images
+  botImage: "https://imagetourl.cloud/jey865he.jpg",
+  menuImage: "https://imagetourl.cloud/9eumy3kr.jpg",
+  ownerImage: "https://imagetourl.cloud/jey865he.jpg",
+  
   footer: "© MADE BY RODGERS"
 };
 

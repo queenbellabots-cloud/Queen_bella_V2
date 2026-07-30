0bc3b784c2d3867148f8c2d4931dd6e27cdf88bb
queenbellabots-cloud <queenbellabots@gmail.com>
2026-07-30 17:12:54 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,26 +1,15 @@
 const settings = {
   prefix: ".",
-  packname: '👑 QUEEN BELLA MD 👑',
-  author: 'Dev RODGERS',
   botName: "👑 QUEEN BELLA MD V1",
-  botOwner: '𝐑𝐎𝐃𝐆𝐄𝐑𝐒 𝐎𝐍𝐘𝐀𝐍𝐆𝐎',
-  ownerNumber: '254755660053',
-  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  botOwner: "𝐑𝐎𝐃𝐆𝐄𝐑𝐒 𝐎𝐍𝐘𝐀𝐍𝐆𝐎",
+  ownerNumber: "254755660053",
   commandMode: "public",
-  maxStoreMessages: 20,
-  storeWriteInterval: 10000,
-  description: "QUEEN BELLA MD - A Multi-Device WhatsApp Bot By Dev RODGERS",
-  version: "1.0.0",
-  updateZipUrl: "https://github.com/YOUR_USERNAME/QUEEN-BELLA-MD/archive/refs/heads/main.zip",
-  usePairingCode: true,  // 👈 CHANGE THIS TO TRUE
-  // 👇 YOUR CHANNEL INFO
-  channelId: '120363423209691396@newsletter',
-  channelName: '👑 QUEEN BELLA MD 👑',
-  channelLink: 'https://whatsapp.com/channel/0029VbBR3ib3LdQQlEG3vd1x',
-  supportGroup: 'https://chat.whatsapp.com/L4TfGq6jXsR3pLbRkStcj8',
-  menuImage: 'https://i.imgur.com/687ZxLW.jpeg',
-  themeEmoji: '👑',
-  footer: '> © MADE BY RODGERS'
+  usePairingCode: true,
+  channelId: "120363423209691396@newsletter",
+  channelName: "👑 QUEEN BELLA MD 👑",
+  channelLink: "https://whatsapp.com/channel/0029VbBR3ib3LdQQlEG3vd1x",
+  menuImage: "https://i.imgur.com/687ZxLW.jpeg",
+  footer: "© MADE BY RODGERS"
 };
 
 global.prefix = settings.prefix;
